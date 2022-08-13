6f3daf69a1eeb41d2cfc1fcf4b57f5065e39ab85
VedantPol <polvedant7@gmail.com>
2022-08-13 10:50:33 +0530

    changed the lock_dir to permdir as its more stable

--- xwiki-jetty/scripts/start.sh
+++ xwiki-jetty/scripts/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-$SNAP_DATA/xwiki-platform-distribution-jetty-hsqldb-13.10.8/start_xwiki.sh -ld ./
+$SNAP_DATA/xwiki-platform-distribution-jetty-hsqldb-13.10.8/start_xwiki.sh -ld ./data/
